81cea2bf6a1e15f1ff1df955c98895b8573c02e3
Andrew Fake <afake518@gmail.com>
2016-02-05 01:30:49 -0500

    Reworked Vagrantfile and provisioning

--- app/setup.sh
+++ app/setup.sh
@@ -2,7 +2,5 @@ sudo apt-get -y update
 sudo apt-get -y install build-essential
 curl -so node.tar.gz https://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-x64.tar.xz
 sudo tar -C /usr/local --strip-components 1 -xvf node.tar.gz && rm node.tar.gz
-cd node-led
-sudo npm install express-generator -g && express -ec --git --force
-npm install --no-bin-links
-echo "alias startNodeServer='DEBUG=node-led:*;(cd ~/node-led/ && npm start)'" >> /home/vagrant/.bash_aliases
+sudo npm install node-pre-gyp -g
+cd app && npm install --no-bin-links
